e89c4a34f856a8fd7a0583274d911b72bbe3d870
Kato Charles <kcharles52@gmail.com>
2019-06-17 15:07:06 +0000

    - add scanner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Scanner from './Scanner'
+import Result from './Result'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            scanning: false,
+            results: []
+        }
+    }
+
+    _scan = () => {
+        this.setState({ scanning: !this.state.scanning })
+    }
+
+    _onDetected = (result) => {
+        this.setState({ results: this.state.results.concat([result]) })
+    }
+
+    render() {
+
+        return (
+            <div></div>
+        );
+    }
 }
-
-export default App;
